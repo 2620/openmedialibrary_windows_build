ad1b53ffad90981b025b4e2f9799f04902c35e16
j <j@mailb.org>
2019-01-10 18:52:56 +0530

    call ensurepip

--- build.sh
+++ build.sh
@@ -7,13 +7,13 @@ PREFIX="$BASE/$NAME"
 
 curl -sO https://www.python.org/ftp/python/${PYTHON_VERSION}/python-${PYTHON_VERSION}.post1-embed-amd64.zip
 curl -sO http://blog.alivate.com.au/wp-content/uploads/2018/10/poppler-0.68.0_x86.7z
-curl -sO https://www.torproject.org/dist/torbrowser/8.0.4/tor-win32-0.3.4.9.zip
+curl -sLO https://dist.torproject.org/torbrowser/8.0.4/tor-win32-0.3.4.9.zip
 
 ls -lah
 
 mkdir -p "$PREFIX"
 cd "$PREFIX"
-unzip "$BASE/python-${PYTHON_VERSION}.post1-embed-amd64.zip"
+7z e "$BASE/python-${PYTHON_VERSION}.post1-embed-amd64.zip"
 mkdir poppler
 cd poppler
 7z e "$BASE/poppler-0.68.0_x86.7z"
@@ -26,6 +26,10 @@ rm -rf poppler
 7z e "$PREFIX/tor-win32-0.3.4.9.zip"
 mv Tor tor
 rm -rf Data
+
+./python3.exe -m ensurepip
+
+ls -lah
 
 ls Scripts
 Scripts/pip3.7.exe install -r requirements.txt
