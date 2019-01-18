cf0e629771205ca6d3065d9e28aa405f03ee8d8b
j <j@mailb.org>
2019-01-18 13:22:11 +0530

    remove tests/docs

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
-#
-#set -e
+set -e
 PYTHON_VERSION=3.7.2
 BASE=`pwd`
 NAME=platform_win64
@@ -12,10 +11,8 @@ curl -sO https://bootstrap.pypa.io/get-pip.py
 ls -lah
 
 choco install python
-
-mkdir -p "$PREFIX"
+cp -r C:\\Python37 "$PREFIX"
 cd "$PREFIX"
-cp -r C:\\Python37 python37
 mkdir poppler
 cd poppler
 7z e "$BASE/poppler-0.68.0_x86.7z"
@@ -30,8 +27,8 @@ mv Tor tor2
 mv tor2 tor
 rm -rf Data
 
-"$PREFIX/python37/Scripts/pip.exe" install -r "$BASE/requirements.txt"
-ls python37/Lib/site-packages
+"$PREFIX/Scripts/pip.exe" install -r "$BASE/requirements.txt"
+ls Lib/site-packages
 
 cd "$BASE"
 7z a "$PREFIX.zip" "$NAME"
