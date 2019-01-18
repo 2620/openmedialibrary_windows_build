76ccaad98c2b34b07906368e6496034e1fe80913
j <j@mailb.org>
2019-01-18 13:46:52 +0530

    expand variables

--- build.sh
+++ build.sh
@@ -4,8 +4,6 @@ BASE=`pwd`
 NAME=platform_win64
 PREFIX="$BASE/$NAME"
 
-curl -sO http://blog.alivate.com.au/wp-content/uploads/2018/10/poppler-0.68.0_x86.7z
-curl -sLO https://dist.torproject.org/torbrowser/8.0.4/tor-win32-0.3.4.9.zip
 curl -sO https://bootstrap.pypa.io/get-pip.py
 
 ls -lah
@@ -14,9 +12,17 @@ choco install python
 cp -r C:\\Python37 "$PREFIX"
 cd "$PREFIX"
 rm -rf Doc Lib/test
+set
+echo $VCINSTALLDIR
+export CL=-FI"$VCINSTALLDIR\\include\\stdint.h $CL"
 
-mkdir poppler
-cd poppler
+"$PREFIX/Scripts/pip.exe" install -r "$BASE/requirements.txt"
+ls Lib/site-packages
+
+cd "$BASE"
+curl -sO http://blog.alivate.com.au/wp-content/uploads/2018/10/poppler-0.68.0_x86.7z
+mkdir "$PREFIX/poppler"
+cd "$PREFIX/poppler"
 7z e "$BASE/poppler-0.68.0_x86.7z"
 cd "$PREFIX"
 mv poppler/*.dll .
@@ -24,15 +30,13 @@ mv poppler/pdftocairo.exe .
 mv poppler/pdftotext.exe .
 rm -rf poppler
 
+cd "$BASE"
+curl -sLO https://dist.torproject.org/torbrowser/8.0.4/tor-win32-0.3.4.9.zip
+cd "$PREFIX"
 unzip "$BASE/tor-win32-0.3.4.9.zip"
 mv Tor tor2
 mv tor2 tor
 rm -rf Data
-
-export CL=-FI"%VCINSTALLDIR%Tools\\MSVC\\14.11.25503\\include\\stdint.h"
-
-"$PREFIX/Scripts/pip.exe" install -r "$BASE/requirements.txt"
-ls Lib/site-packages
 
 cd "$BASE"
 7z a "$PREFIX.zip" "$NAME"
